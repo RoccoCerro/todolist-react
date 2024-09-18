2ceb6272fce2a8d9fe9283e81ca43bd0ea994c09
RoccoCerro <rcerro02@gmail.com>
2024-09-18 13:37:09 +0200

    Add: comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,40 @@ import { useState, useEffect } from 'react'
 import HeaderComponent from './components/HeaderComponent'
 
 function App() {
+  
   const [todos, setTodos] = useState(() => {
+    // Recupero la lista di todos dal localStorage
     const savedTodos = localStorage.getItem('todos');
+    // Ritorno la lista di todos se presente
     return savedTodos ? JSON.parse(savedTodos) : [];
   })
   const [newTodo, setNewTodo] = useState('')
-  // const [todosLocal, setTodosLocal] = useState([])
 
   useEffect(() => {
+    // Aggiorno la lista di todos nel localStorage ogni volta che l'array di todos cambia
     localStorage.setItem('todos' , JSON.stringify(todos))
     
   }, [todos])
 
+  // Recupero il valore dell'input
   const handleNewTodoChange = (event) => {
     // event.preventDefault()
     setNewTodo(event.target.value)
-    console.log(newTodo)
   }
 
+  // Aggiungo un todo
   const handleAddTodo = (event) => {
     event.preventDefault()
     if (newTodo === '') return
     setTodos([...todos, newTodo])
-    setNewTodo('')
-    console.log(todos)   
+    setNewTodo('') 
   }
 
+  // Elimino un todo
   const handleTodoDelete = (index) => {
     const newTodos = [...todos]
     newTodos.splice(index,1)
     setTodos(newTodos)
-
-
   }
 
   return (
